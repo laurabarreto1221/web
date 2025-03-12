fd2be15f7b6dd48a674d4f0404d1ffa924c11988
LauraBarreto <laurabarreto1221@gmail.com>
2025-03-11 23:22:24 -0500

    Add files via upload

---
+++ Adivinar,numero,aleatorio.php
@@ -0,0 +1,289 @@
+<html>
+<head></head>
+<body>
+<?php
+$num = rand(1, 50);
+echo "<br>";
+
+if ($num <= 50) {
+    echo "el numero esta entre 1 y 50";
+} else {
+    echo "el numero esta entre 51 y 100";
+}
+echo "<br>";
+
+if ($num <= 25) {
+    echo "el numero esta entre 1 y 25";
+} else {
+    echo "el numero esta entre 26 y 50";
+}
+echo "<br>";
+
+if ($num <= 15) {
+    echo "el numero esta entre 1 y 15";
+} else {
+    echo "el numero esta entre 16 y 25";
+}
+echo "<br>";
+
+if ($num <= 7) {
+    echo "el numero esta entre 1 y 7";
+} else {
+    echo "el numero esta entre 8 y 15";
+}
+echo "<br>";
+
+if ($num <= 3) {
+    echo "el numero esta entre 1 y 3";
+} else {
+    echo "el numero esta entre 4 y 7";
+}
+echo "<br>";
+
+if ($num <= 2) {
+    echo "el numero es 1 o 2";
+} else {
+    echo "el numero es 3";
+}
+echo "<br>";
+
+if ($num >= 16 && $num <= 25) {
+    echo "el numero esta entre 16 y 25";
+} else {
+    echo ".";
+}
+echo "<br>";
+
+if ($num >= 16 && $num <= 20) {
+    echo "el numero esta entre 16 y 20";
+} else {
+    echo "el numero esta entre 21 y 25";
+}
+echo "<br>";
+
+if ($num >= 16 && $num <= 18) {
+    echo "el numero esta entre 16 y 18";
+} else {
+    echo "el numero esta entre 19 y 20";
+}
+echo "<br>";
+
+if ($num >= 16 && $num <= 17) {
+    echo "el numero es 16 o 17";
+} else {
+    echo "el numero es 18";
+}
+echo "<br>";
+
+if ($num >= 8 && $num <= 10) {
+    echo "el numero esta entre 8 y 10";
+} else {
+    echo "el numero esta entre 11 y 15";
+}
+echo "<br>";
+
+if ($num >= 8 && $num <= 9) {
+    echo "el numero es 8 o 9";
+} else {
+    echo "el numero es 10";
+}
+echo "<br>";
+
+if ($num >= 11 && $num <= 12) {
+    echo "el numero es 11 o 12";
+} else {
+    echo "el numero esta entre 13 y 15";
+}
+echo "<br>";
+
+if ($num >= 13 && $num <= 14) {
+    echo "el numero es 13 o 14";
+} else {
+    echo "el numero es 15";
+}
+echo "<br>";
+
+if ($num >= 21 && $num <= 23) {
+    echo "el numero esta entre 21 y 23";
+} else {
+    echo "el numero esta entre 24 y 25";
+}
+echo "<br>";
+
+if ($num >= 21 && $num <= 22) {
+    echo "el numero es 21 o 22";
+} else {
+    echo "el numero es 23";
+}
+echo "<br>";
+
+if ($num >= 21 && $num <= 23) {
+    echo "el numero esta entre 21 y 23";
+} else {
+    echo "el numero esta entre 24 y 25";
+}
+echo "<br>";
+
+if ($num >= 26 && $num <= 32) {
+    echo "el numero esta entre 26 y 32";
+} else {
+    echo "el numero esta entre 33 y 39";
+}
+echo "<br>";
+
+if ($num >= 26 && $num <= 29) {
+    echo "el numero esta entre 26 y 29";
+} else {
+    echo "el numero esta entre 30 y 32";
+}
+echo "<br>";
+
+if ($num >= 26 && $num <= 27) {
+    echo "el numero es 26 o 27";
+} else {
+    echo "el numero esta es 28 o 29";
+}
+echo "<br>";
+
+if ($num >= 30 && $num <= 31) {
+    echo "el numero es 30 o 31";
+} else {
+    echo "el numero es 32";
+}
+echo "<br>";
+
+if ($num >= 33 && $num <= 36) {
+    echo "el numero esta entre 33 y 36";
+} else {
+    echo "el numero esta entre 37 y 39";
+}
+echo "<br>";
+
+if ($num >= 33 && $num <= 34) {
+    echo "el numero es 33 o 34";
+} else {
+    echo "el numero es 35 o 36";
+}
+echo "<br>";
+
+if ($num >= 37 && $num <= 38) {
+    echo "el numero es 37 o 38";
+} else {
+    echo "el numero es 39";
+}
+echo "<br>";
+
+if ($num >= 40 && $num <= 50) {
+    echo "el numero esta entre 40 y 50";
+} else {
+    echo "//";
+}
+echo "<br>";
+
+if ($num >= 40 && $num <= 45) {
+    echo "el numero esta entre 40 y 45";
+} else {
+    echo "el numero esta entre 46 y 50";
+}
+echo "<br>";
+
+if ($num >= 40 && $num <= 43) {
+    echo "el numero esta entre 40 y 43";
+} else {
+    echo "el numero esta entre 44 y 45";
+}
+echo "<br>";
+
+if ($num >= 40 && $num <= 42) {
+    echo "el numero esta entre 40 y 42";
+} else {
+    echo "el numero es 43";
+}
+echo "<br>";
+
+if ($num >= 44 && $num <= 45) {
+    echo "el numero es 44 o 45";
+} else {
+    echo "";
+}
+echo "<br>";
+
+if ($num >= 46 && $num <= 48) {
+    echo "el numero esta entre 46 y 48";
+} else {
+    echo "el numero es 49 o 50";
+}
+echo "<br>";
+
+if ($num >= 46 && $num <= 47) {
+    echo "el numero es 46 o 47";
+} else {
+    echo "el numero es 48";
+}
+echo "<br>";
+
+if ($num == 1) {
+    echo "el numero es 1";
+} else {
+    echo "";
+}
+echo "<br>";
+
+if ($num <= 2) {
+    echo "el numero es 2";
+} else {
+    echo "";
+}
+echo "<br>";
+
+if ($num == 4) {
+    echo "el numero es 4";
+} else {
+    echo "";
+}
+echo "<br>";
+
+if ($num == 5) {
+    echo "el numero es 5";
+} else {
+    echo "";
+}
+echo "<br>";
+
+if ($num == 6) {
+    echo "el numero es 6";
+} else {
+    echo "";
+}
+echo "<br>";
+
+if ($num == 7) {
+    echo "el numero es 7";
+} else {
+    echo "";
+}
+echo "<br>";
+
+if ($num == 8) {
+    echo "el numero es 8";
+} else {
+    echo "";
+}
+echo "<br>";
+
+if ($num == 9) {
+    echo "el numero es 9";
+} else {
+    echo "";
+}
+echo "<br>";
+
+if ($num <= 25) {
+    echo "el numero esta entre 1 y 25";
+} else {
+    echo "el numero esta entre 26 y 50";
+}
+echo "<br>";
+?>
+</body>
+</html>
