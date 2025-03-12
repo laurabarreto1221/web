94186a7a2471b0d7a30e178e4fa755471731d49f
LauraBarreto <laurabarreto1221@gmail.com>
2025-03-11 22:56:07 -0500

    Add files via upload

---
+++ variables de tipo string/ejercicio10.php
@@ -0,0 +1,9 @@
+<html>
+<head></head>
+<body>
+<?php
+$p1=85;
+$p2=90;
+$p3=78;
+echo"El estudiante obtuvo las siguientes puntuaciones:$p1, $p2 y $p3";
+?>
---
+++ variables de tipo string/ejercicio6.php
@@ -0,0 +1,9 @@
+<html>
+<head></head>
+<body>
+<?php
+$le=3;
+$lf=5;
+$lm=2;
+echo"En Enero se leyeron $le, en Febrero $lf y en Marzo $lm ";
+?>
