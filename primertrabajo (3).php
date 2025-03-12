d09c3b095f70673e4ae5e9594a25e6db006cdb77
LauraBarreto <laurabarreto1221@gmail.com>
2025-03-11 23:24:06 -0500

    Add files via upload

---
+++ primertrabajo (3).php
@@ -0,0 +1,48 @@
+<html>
+<head></head>
+<body>
+ <?php
+ echo "Bienvenido a clase de programacion";
+echo"<br>";
+echo"Me llamo Laura Valentina Barreto Roa";
+echo"<br>";
+echo"tengo 18 años";
+echo"<br>";
+echo"Actualmente estudio ingenieria industrial en la Sergio Arboleda";
+echo"<br>";
+echo"Me gusta todo tipo de musica";
+echo"<br>";
+echo"En mis tiempos libres hago de todo";
+echo"<br>";
+echo"Dibujo, leo, escribo, aprendo a tocar instrumentos...";
+echo"<br>";
+echo"Soy una persona muy activa";
+echo"<br>";
+echo"Me encanta hacer deportes";
+echo"<br>";
+echo"Vivo por el baloncesto";
+echo"<br>";
+echo"He practicado 3 deportes ";
+echo"<br>";
+echo"baoncesto,volleyball y patinaje de carreras";
+echo"<br>";
+echo"He practicado baloncesto por 6 años";
+echo"<br>";
+echo"ayer compre una bicicleta";
+echo"<br>";
+echo"El sabado bloquee a un amigo que ya no es amigo ";
+echo"<br>";
+echo"Me rompi los ligamentos y los meiscos de la rodilla";
+echo"<br>";
+echo"Me gane una beca con jovenes a la e";
+echo"<br>";
+echo"En año nuevo un perro me orino la espalda";
+echo"<br>";
+echo"Tengo el pelo MUY corto";
+echo"<br>";
+echo"Me operaron los oidos a los 4 años";
+echo"<br>";
+echo"Soy muy dislexica";
+ ?>
+</body>
+</html>
